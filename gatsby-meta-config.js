aaad41c280180684ef6606c76062baf3c3821f74
HeewonKim <thisisheewonkim@gmail.com>
2024-12-03 10:06:36 +0900

    🌱 Started my blogfolio website

---
+++ gatsby-meta-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `thisisheewonkim`,
+  description: `Blog posted about ...`,
+  author: `Heewon Kim`,
+  introduction: `Product Designer based in Seoul, South KR`,
+  siteUrl: `https://thisisheewonkim.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `thisisheewonkim`, // Your GitHub account
+    medium: `@thisisheewonkim`, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: `thisisheewonkim`, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'Jthisisheewonkim/thisisheewonkim.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '0', // Add your google analytics tranking ID
+  ad: '', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+}
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import './index.scss'
+
+export const Footer = () => (
+  <footer className="footer">
+    ©<a href="https://github.com/thisisheewonkim">thisisheewonkim</a>, All
+    rights reserved
+  </footer>
+)
